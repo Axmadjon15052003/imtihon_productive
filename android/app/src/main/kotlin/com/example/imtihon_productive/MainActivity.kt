b116751fe8840adf771b8ff4e1dfc97070f990a3
Ahmadjon <axmadjonbakirov2@gmail.com>
2024-03-19 10:10:37 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/imtihon_productive/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.imtihon_productive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/lib/assets/icons.dart
@@ -0,0 +1,11 @@
+const authen = 'assets/svg/';
+
+sealed class AppIcons {
+  static const logo = '${authen}logo.svg';
+  static const apple = '${authen}apple.svg';
+  static const eyeOff = '${authen}eye_off.svg';
+  static const eyeOn = '${authen}eye_on.svg';
+  static const facebook = '${authen}facebook.svg';
+  static const google = '${authen}google.svg';
+
+}
